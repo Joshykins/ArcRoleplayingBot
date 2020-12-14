685c57778dbb179a4eed0210f41ad338ecad3076
Josh R <jroelle@blaze.me>
2020-12-13 17:56:25 -0800

    Added color env

--- src/util/enviromentalVariables.ts
+++ src/util/enviromentalVariables.ts
@@ -10,9 +10,14 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+//Color type import
+let processColor : any = process.env.BOT_COLOR;
+if(processColor) {
+    processColor = Number(processColor);
+}
 export const discordBotConfig : DiscordBotConfig = {
     token: process.env.BOT_TOKEN,
-    color: 0x3c50ff,
+    color: processColor || 0x3c50ff,
     profilePic:"http://www.womenfitness.net/wp/wp-content/uploads/2016/07/rubyro-1-500x500.jpg",
     notificationsChannel: "607132389243748382",
     adminRole: "607162419147702283"
